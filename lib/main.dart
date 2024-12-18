50804d34eea0ae1bdd0ddf46f2253a9db90100fb
alawaashez2 <mawasher@moj.gov.sa>
2024-12-18 16:46:10 +0300

    add dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:day10_database/pages/list_page.dart';
 import 'package:day10_database/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async{
    WidgetsFlutterBinding.ensureInitialized();
+   await dotenv.load(fileName: ".env");
  await Supabase.initialize(
-    url: "",
-    anonKey: ""
+    url:dotenv.env["supabseUrl"]! ,
+    anonKey:dotenv.env["supabseKey"]! ,
   );
   runApp(const MainApp());
 }
